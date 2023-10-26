cc1eb12333ddead93835bcbaa85af9a1b99f359c
ZackarySantana <zackzackyack@gmail.com>
2023-10-26 17:30:12 -0400

    Checkout commit command

--- src/cmd/checkoutCommit.ts
+++ src/cmd/checkoutCommit.ts
@@ -1,8 +1,28 @@
+import { exec } from "child_process";
+import { showError } from "../pkg/utils";
 import { GroveContext } from "src/types";
-import * as vscode from "vscode";
+import { window } from "vscode";
 
 export function initCheckoutCommit(context: GroveContext) {
     return async (commitHash: string) => {
-        // TODO: Checkout commit hash
+        exec(
+            `git fetch --all && git checkout ${commitHash}`,
+            {
+                cwd: context.workspaceFolder.uri.path,
+            },
+            (err, stdout, stderr) => {
+                console.log("stdout: " + stdout);
+                console.log("stderr: " + stderr);
+                if (err) {
+                    showError(
+                        `We could not checkout that commit, are you in the right project? ${err}`,
+                    );
+                } else {
+                    window.showInformationMessage(
+                        `You have checked out ${commitHash}`,
+                    );
+                }
+            },
+        );
     };
 }
